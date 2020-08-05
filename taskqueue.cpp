99df9216436a02aafef7b393bfef33cfbeb29474
Shinji Ogaki <shinji.ogaki@gmail.com>
2020-08-05 22:15:18 +0900

    taskque with control variable

--- taskqueue.cpp
+++ taskqueue.cpp
@@ -4,7 +4,6 @@
 #include <mutex>
 
 // small parallel BVH (object splitting) construction simulator (c) shinji ogaki
-
 struct Range
 {
 	int Start;
@@ -28,6 +27,8 @@ struct Range
 	}
 };
 
+//#define BUSY
+#ifdef BUSY
 struct BVH
 {
 	static const int LeafCount = 4;
@@ -40,7 +41,7 @@ struct BVH
 	int MaxElems;
 	int NumTasks;
 
-	void InsertRange(const Range &r)
+	void InsertRange(const Range& r)
 	{
 		std::lock_guard<std::mutex> lock(Mutex);
 
@@ -62,7 +63,7 @@ struct BVH
 		}
 	}
 
-	bool GetRange(Range &r)
+	bool GetRange(Range& r)
 	{
 		std::lock_guard<std::mutex> lock(Mutex);
 
@@ -82,7 +83,7 @@ struct BVH
 		return (Progress < MaxElems);
 	}
 
-	void ProcessRange(const Range &r)
+	void ProcessRange(const Range& r)
 	{
 		// do something expensive
 		std::this_thread::sleep_for(std::chrono::milliseconds(10 + r.Length()));
@@ -106,18 +107,126 @@ struct BVH
 		// create threads
 		for (auto i = 0u; i < num_threads; ++i)
 			threads[i] = std::thread([&]
-		{
-			Range p;
-			while (UnderConstruction())
-				if (GetRange(p))
-					ProcessRange(p);
-		});
+				{
+					Range p;
+					while (UnderConstruction())
+						if (GetRange(p))
+							ProcessRange(p);
+				});
 
 		// wait!
 		for (auto& thread : threads)
 			thread.join();
 	}
 };
+#else
+struct BVH
+{
+	static const int LeafCount = 4;
+
+	Range Ranges[1024];
+
+	std::mutex Mutex;
+	std::condition_variable CV;
+
+	int Progress;
+	int MaxElems;
+	int NumTasks;
+
+	bool IsDone() const
+	{
+		// Yuichi Sayama's idea
+		return (MaxElems <= Progress);
+	}
+
+	void InsertRange(const Range& r)
+	{
+		std::unique_lock<std::mutex> lock(Mutex);
+
+		// Node
+		if (LeafCount < r.Length())
+		{
+			// create inner node
+			const auto partition = r.Partition();
+			Ranges[NumTasks + 0].Set(r.Start, partition);
+			Ranges[NumTasks + 1].Set(partition, r.End);
+
+			// two tasks added
+			NumTasks += 2;
+			CV.notify_all();
+		}
+		// Leaf
+		else
+		{
+			Progress += r.Length();
+
+			// tell everybody if construction is done
+			if (IsDone())
+			{
+				CV.notify_all();
+			}
+		}
+	}
+
+	bool GetRange(Range& r)
+	{
+		std::unique_lock<std::mutex> lock(Mutex);
+		CV.wait(lock, [&]
+			{
+				return (0 < NumTasks) || IsDone();
+			});
+
+		if (IsDone())
+		{
+			return false;
+		}
+
+		// NumTasks=0 doesn't necessarily mean that build is done
+		--NumTasks;           // single task is taken
+		r = Ranges[NumTasks]; // task
+		return true;
+	}
+
+	void ProcessRange(const Range& r)
+	{
+		// do something expensive
+		std::this_thread::sleep_for(std::chrono::milliseconds(10 + r.Length()));
+
+		// insert new node & ranges
+		InsertRange(r);
+	}
+
+	void Build(const int max_elems)
+	{
+		const auto num_threads = std::thread::hardware_concurrency();
+
+		Progress = 0;
+		NumTasks = 1;
+		MaxElems = max_elems;
+		Ranges[0].Set(0, max_elems);
+
+		// alloc
+		std::vector<std::thread> threads(num_threads);
+
+		// create threads
+		for (auto i = 0u; i < num_threads; ++i)
+			threads[i] = std::thread([&]
+				{
+					Range p;
+					while (GetRange(p))
+					{
+						ProcessRange(p);
+					}
+				});
+
+		// wait!
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+	}
+};
+#endif
 
 int main()
 {
